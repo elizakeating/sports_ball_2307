f0b312a6f1e1feb4f5b50aeb5451890b9c74d6da
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-13 09:32:16 -0600

    Complete iteration 1

---
+++ lib/player.rb
@@ -0,0 +1,21 @@
+class Player
+  attr_reader :first_name, 
+              :last_name, 
+              :monthly_cost, 
+              :contract_length, 
+              :total_cost, 
+              :nickname
+
+  def initialize(name, monthly_cost, contract_length)
+    @first_name = name.split[0]
+    @last_name = name.split[1]
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @total_cost = monthly_cost * contract_length
+    @nickname = nil
+  end
+
+  def set_nickname!(nickname)
+    @nickname = nickname
+  end
+end
