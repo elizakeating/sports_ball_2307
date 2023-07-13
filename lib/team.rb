235da2004d21d6fcf304a5282aacbdff4dc3d9a3
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-13 10:33:42 -0600

    Finish iteration 3

--- lib/team.rb
+++ lib/team.rb
@@ -17,4 +17,34 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    long_contract = @roster.select do |player|
+      player if player.contract_length/12 > 2
+    end
+    long_contract_sorted = long_contract.sort_by do |player|
+      player.contract_length
+    end
+  end
+
+  def short_term_players
+    short_contract = @roster.select do |player|
+      player if player.contract_length/12 <= 2
+    end
+    short_contract_sorted = short_contract.sort_by do |player|
+      player.contract_length
+    end
+  end
+
+  def total_value
+    tot_value = 0
+    @roster.each do |player|
+      tot_value += player.total_cost
+    end
+    tot_value
+  end
+
+  def details
+    {"total_value" => self.total_value, "player_count" => @roster.size}
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -36,4 +36,100 @@ RSpec.describe Team do
     expect(team.roster).to eq([player_1, player_2])
     expect(team.player_count).to eq(2)
   end
+
+  it 'returns long term players' do
+    team = Team.new("Dodgers", "Los Angeles")
+    expect(team).to be_a(Team)
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.long_term_players).to eq([player_1, player_3])
+  end
+
+  it 'returns short term player' do
+    team = Team.new("Dodgers", "Los Angeles")
+    expect(team).to be_a(Team)
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.short_term_players).to eq([player_4, player_2])
+  end
+
+  it 'returns total team value' do
+    team = Team.new("Dodgers", "Los Angeles")
+    expect(team).to be_a(Team)
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq(85200000)
+  end
+
+  it 'returns team details' do
+    team = Team.new("Dodgers", "Los Angeles")
+    expect(team).to be_a(Team)
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a(Player)
+
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a(Player)
+
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    expect(player_3).to be_a(Player)
+
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+    expect(player_4).to be_a(Player)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+    
+    expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+  end
 end
